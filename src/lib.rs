02e4d4abafa6a9a68a00cfe5c63211c690b5b2d5
Nikolas Wipper <nwipper@mozilla.com>
2026-07-30 12:03:48 +0200

    fix: Initialize NSS in read-write mode

--- src/lib.rs
+++ src/lib.rs
@@ -161,7 +161,7 @@ fn init_once(db: Option<PathBuf>) -> Res<NssLoaded> {
                 empty.as_ptr(),
                 empty.as_ptr(),
                 nss::SECMOD_DB.as_ptr().cast(),
-                nss::NSS_INIT_READONLY,
+                0,
             )
         })?;
 
